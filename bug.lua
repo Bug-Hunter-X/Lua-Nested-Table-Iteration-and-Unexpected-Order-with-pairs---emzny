1e059e21933640e3dcdd5dd6dd36ae99bf6ac3a7
BugHunter-X <askzenai@gmail.com>
2025-02-09 07:04:07 +0100

    Add bug.lua file

---
+++ bug.lua
@@ -0,0 +1,24 @@
+local function foo(t)
+  for k, v in pairs(t) do
+    if type(v) == "table" then
+      foo(v)
+    end
+  end
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+foo(t)
+
+--[[The above code will not modify the original table t. If you need to modify the original table t, you can use the following code:
+
+local function foo(t)
+  for k, v in pairs(t) do
+    if type(v) == "table" then
+      foo(v)
+    end
+  end
+end
+
+local t = {a = 1, b = {c = 2, d = {e = 3}}}
+foo(t)
+]]
